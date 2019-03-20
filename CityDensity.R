a97b1d68c358958ff34001c2d2a433eb6eb24dee
kcpagaduan@globe.com.ph <10012227@globetel.com>
2019-03-20 09:29:48 +0800

    City Density

---
+++ CityDensity.R
@@ -0,0 +1,26 @@
+population <- read.csv("population.csv")
+regionArea <- read.csv("regionarea.csv")
+regionAreaAndPopulation <- merge(population, regionArea, by = "Region")
+library(dplyr)
+
+AreaOfCityPerRegion <- regionArea$Area/count(regionAreaAndPopulation, Region)$n
+AreaOfCityPerRegion <- data.frame(regionArea$Region, AreaOfCityPerRegion)
+PopulationByCity <- aggregate(population$Population, by = list(population$CityProvince), FUN = sum, na.rm = TRUE)
+colnames(PopulationByCity) <- c('CityProvince', 'Total Population')
+colnames(AreaOfCityPerRegion) <- c('Region', 'Area')
+
+merge(population,
+      PopulationByCity, by = 'CityProvince')
+
+CityProvinceRegion <- data.frame(population$CityProvince,population$Region)
+colnames(CityProvinceRegion) <- c('CityProvince', 'Region')
+
+PopulationByRegionCity <- merge(CityProvinceRegion, PopulationByCity, by='CityProvince')
+PopulationByRegionCity<-distinct(PopulationByRegionCity)
+
+
+FinalCityDensity <- merge(PopulationByRegionCity,AreaOfCityPerRegion, by = 'Region')
+FinalCityDensity$CityDensity <- FinalCityDensity$`Total Population`/FinalCityDensity$Area
+
+FinalCityDensity <- arrange(FinalCityDensity, desc(CityDensity))
+write.csv(file = "Top5TestCity.csv",FinalCityDensity[1:5,])
